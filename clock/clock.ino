bbfc37284f485ea6302e5628b6f46f198e0d3b3b
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-11-23 18:02:53 -0700

    maybe fixed folder structure

--- clock/clock.ino
+++ clock/clock.ino
@@ -1,7 +1,11 @@
-#include "./../"
+#include "./src/14-segment-digit/fourteen_segment_digit.h"
+
+CRGB arr1[39];
+fourteen_segment_digit digit1; 
 
 void setup() {
-
+    digit1.begin(19, arr1, 3, 3, 1);
+    digit1.erase();
 }
 
 
