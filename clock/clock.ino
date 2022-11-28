7738974951bdb86ec9994c4e01132580c4d3db21
Tyler-Scripps <tylerscripps@gmail.com>
2022-11-27 21:48:13 -0700

    update submodule with character display

--- clock/clock.ino
+++ clock/clock.ino
@@ -10,9 +10,46 @@ void setup() {
   delay(1000);
   digit1.fill(50, 50, 50);
   FastLED.show();
+  delay(1000);
+  digit1.erase();
 }
 
 
 void loop() {
+  // for (uint8_t i = 0; i < 14; i++)
+  // {
+  //   digit1.erase();
+  //   digit1.setSegment(i, 50, 50, 50);
+  //   FastLED.show();
+  //   delay(1000);
+  // }
+  // digit1.erase();
+  // digit1.setSegments(0b0011111100000000, 50, 50, 50);
+  // FastLED.show();
+  // delay(2000);
+  // digit1.fill(50,50,50);
+  // FastLED.show();
+  // delay(2000);
 
+  for (byte i = '0'; i <= '9'; i++)
+  {
+    digit1.setChar(i, 50, 0, 0);
+    FastLED.show();
+    delay(1500);
+  }
+  
+  for (byte i = 'a'; i <= 'z'; i++)
+  {
+    digit1.setChar(i, 0, 50, 0);
+    FastLED.show();
+    delay(1500);
+  }
+
+  for (byte i = 'A'; i <= 'Z'; i++)
+  {
+    digit1.setChar(i, 0, 0, 50);
+    FastLED.show();
+    delay(1500);
+  }
+  
 }
