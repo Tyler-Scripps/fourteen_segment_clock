e6ca5e5d4b66aa53525823878d46ff36cadf3c19
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-12-04 01:12:53 -0700

    5v leds

--- clock/clock.ino
+++ clock/clock.ino
@@ -1,11 +1,11 @@
 #include "./src/14-segment-digit/fourteen_segment_digit.h"
 
-CRGB arr1[39];
+CRGB arr1[251];
 fourteen_segment_digit digit1; 
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, 19>(arr1, 39);
-  digit1.begin(arr1, 3, 3, 1);
+  FastLED.addLeds<NEOPIXEL, 19>(arr1, 251);
+  digit1.begin(arr1, 20, 19, 1);
   digit1.erase();
   delay(1000);
   digit1.fill(50, 50, 50);
